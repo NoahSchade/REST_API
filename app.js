4526af47e9d06d2747b5ce16222d9e778782fc6e
NoahSchade <noah9@mail.com>
2019-10-25 21:52:03 -0400

    Updating files

--- app.js
+++ app.js
@@ -1,20 +1,128 @@
 'use strict';
 
+const { sequelize, models } = require('./db');
+// Get references to our models.
+const { User, Course } = models;
+
 // load modules
 const db = require('./db');
 const express = require('express');
 const morgan = require('morgan');
 
-(async () => {
-  await db.sequelize.sync({ force: true });
+// (async () => {
+//   await db.sequelize.sync({ force: true });
 
-  try {
-    await db.sequelize.authenticate();
-    console.log('Connection to the database successful!');
-  } catch (error) {
-    console.error('Error connecting to the database: ', error);
-  }
-})();
+//   try {
+//     await db.sequelize.authenticate();
+//     console.log('Connection to the database successful!');
+//   } catch (error) {
+//     console.error('Error connecting to the database: ', error);
+//   }
+// })();
+
+// Define variables for the people and courses.
+// NOTE: We'll use these variables to assist with the creation
+// of our related data after we've defined the relationships
+// (or associations) between our models.
+let bradBird;
+let vinDiesel;
+let eliMarienthal;
+let craigTNelson;
+let hollyHunter;
+let theIronGiant;
+let theIncredibles;
+
+console.log('Testing the connection to the database...');
+
+// Test the connection to the database.
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Synchronizing the models with the database...');
+
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.info('Adding people to the database...');
+
+    return Promise.all([
+      User.create({
+        firstName: 'Brad',
+        lastName: 'Bird',
+        emailAddress: 'bradBird@gmail.com',
+        password: "qwerty"
+      }),
+      // User.create({
+      //   firstName: 'Vin',
+      //   lastName: 'Diesel',
+      // }),
+      // User.create({
+      //   firstName: 'Eli',
+      //   lastName: 'Marienthal',
+      // }),
+      // User.create({
+      //   firstName: 'Craig T.',
+      //   lastName: 'Nelson',
+      // }),
+      // User.create({
+      //   firstName: 'Holly',
+      //   lastName: 'Hunter',
+      // }),
+    ]);
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    // Update the global variables for the people instances.
+    [bradBird, vinDiesel, eliMarienthal, craigTNelson, hollyHunter] = data;
+
+    console.log('Adding courses to the database...');
+
+    // TODO Include the related user who directed each course.
+
+    return Promise.all([
+      Course.create({
+        userId: bradBird.id,
+        title: 'The Iron Giant',
+        releaseYear: 1999,
+        description: "blah blah blah"
+      }),
+      // Course.create({
+      //   title: 'The Incredibles',
+      //   releaseYear: 2004,
+      // }),
+    ]);
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    // Update the global variables for the course instances.
+    [theIronGiant, theIncredibles] = data;
+
+    // TODO Create the data for each course's actors.
+
+    return Promise.resolve();
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    // TODO Update this query to include any related data.
+
+    return Course.findAll();
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    // TODO Update this query to include any related data.
+
+    return User.findAll();
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, null, 2));
+
+    process.exit();
+  })
+  .catch(err => console.error(err));
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
---
+++ db/models/course.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define('Course', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    estimatedTime: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    materialsNeeded: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+
+  Course.associate = (models) => {
+    Course.belongsTo(models.User, { foreignKey: 'userId' });
+  };
+
+  return Course;
+};
